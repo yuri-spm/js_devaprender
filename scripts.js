d78804421bc1b984c06630ba27b9149996e467bb
Yuri do Monte <yspm.developer@gmail.com>
2024-01-09 08:24:38 -0300

    aula-14

--- scripts.js
+++ scripts.js
@@ -1,7 +1,31 @@
-// Operador Ternário
-// Imagine um cliente, 100 premium, comum
+// Operador Lógico e (&&)
+// Retorna TRUE se os dois operando forem true
 
-let pontos = 200;
-let tipo = pontos > 100 ? 'premium' : 'comum';
+console.log(true && true);
+console.log(false && true);
 
-console.log(tipo);
+// let maiorDeIdade = true;
+// let possuiCarteiraDeTrabalho = true;
+// let podeAplicar = maiorDeIdade && possuiCarteiraDeTrabalho;
+
+// console.log(podeAplicar);
+
+
+// Operador Lógico ou (||)
+// Retornar true se um dos operandos forem true
+
+// let maiorDeIdade = true;
+// let possuiCarteiraDeTrabalho = true;
+// let podeAplicar = maiorDeIdade || possuiCarteiraDeTrabalho;
+// console.log(podeAplicar);
+
+
+// Operador NOT (!)
+
+let maiorDeIdade = false;
+let possuiCarteiraDeTrabalho = false;
+let podeAplicar = maiorDeIdade || possuiCarteiraDeTrabalho;
+console.log('pode aplicar: ', podeAplicar);
+let canditatoRecusado = !podeAplicar;
+
+console.log('Candidato Recusado ',canditatoRecusado);
