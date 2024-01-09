07b6a5205056080b718915278288fe79261f4bd0
Yuri do Monte <yspm.developer@gmail.com>
2024-01-08 22:36:12 -0300

    aula-06

--- scripts.js
+++ scripts.js
@@ -1,18 +1,6 @@
+let familia = [
+    26, 32, 48, 58
+];
 
-let nome = "Yuri"; //string literal
-let idade = 25; //number literal
-let estaAprovado = true; //boolean
-let sobrenome = undefined; //undefined
-
-let pessoa = {
-    nome: 'Yuri',
-    idade: '32',
-    estaAprovado: false,
-    sobrenome: 'do Monte'
-
-};
-
-console.log(pessoa);
-console.log(pessoa.nome)
-
-
+console.log(familia[3]);
+console.log(familia.length);
