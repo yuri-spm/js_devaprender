d2ef07b92ffc10d46bc3f480fd1181ac4327fd57
Yuri do Monte <yspm.developer@gmail.com>
2024-01-09 08:03:58 -0300

    aula-10

--- scripts.js
+++ scripts.js
@@ -1,5 +1,25 @@
-// Operadores Aritiméticos(matemáticos)
-// Operadores de Atribuição
-// Operadores de comparação
-// Operadores Logicos
-// Operadores Bitwise
+// // Operadores Aritiméticos(matemáticos)
+// let salario = 100;
+
+// //+ , - , *, / , **
+
+// console.log(salario + salario);
+// console.log(salario - salario);
+// console.log(salario * salario);
+// console.log(salario / salario);
+// console.log(5 ** salario);
+
+// ++ --
+let idade = 18;
+console.log(idade++);
+
+console.log(idade);
+
+console.logo(++idade);
+
+
+console.log(idade--);
+
+console.log(idade);
+
+console.logo(--idade);
