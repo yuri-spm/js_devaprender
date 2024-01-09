5b0729cf9b525c46e823095412f863ee28b51f64
Yuri do Monte <yspm.developer@gmail.com>
2024-01-08 22:15:02 -0300

    aula-04-Tipagem dinamica

--- scripts.js
+++ scripts.js
@@ -4,4 +4,5 @@ let idade = 25; //number literal
 let estaAprovado = true; //boolean
 let sobrenome = undefined; //undefined
 let corSelecionada = null //Redefinir um valor
- 
+
+//para descobrir o tipo da variavel utilizar typeof
