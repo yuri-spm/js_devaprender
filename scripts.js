69410459639d3c7b824948e4dd095a2ea8d36883
Yuri do Monte <yspm.developer@gmail.com>
2024-01-08 21:52:58 -0300

    aula-01

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// let idade = 5;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+
+// let nome = 'Yuri do Monte';
+// console.log(nome);
+
+let nome = 'Yuri do Monte';
+let idade = 32;
+let profissiao = 'Desenvolvedor';
+
+console.log(nome, idade, profissiao)
