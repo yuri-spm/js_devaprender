baec76ca33ad6502cb4d9bda1f07f8c6d48052e6
Yuri do Monte <yspm.developer@gmail.com>
2024-01-08 22:44:29 -0300

    aula-07

--- scripts.js
+++ scripts.js
@@ -1,6 +1,29 @@
-let familia = [
-    26, 32, 48, 58
-];
+let corSite = "azul";
+// function resetarCor(){
+//     corSite = "";
 
-console.log(familia[3]);
-console.log(familia.length);
+
+// };
+
+//  function resetarCor(cor){
+//     corSite = cor;
+
+//  };
+
+// console.log(corSite);
+// resetarCor();
+// console.log(corSite);
+
+
+// console.log(corSite);
+// resetarCor("vermelho");
+// console.log(corSite);
+
+function resetarCor(cor, tonalidade){
+    corSite = cor + ' ' + tonalidade;
+ };
+
+ console.log(corSite);
+ resetarCor("verde" , "claro");
+ console.log(corSite);
+
