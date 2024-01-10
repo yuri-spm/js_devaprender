f64dfeee62cf8781c7889df693f1e3d96bec3b9c
Yuri do Monte <yspm.developer@gmail.com>
2024-01-09 23:36:59 -0300

    aula-23

--- scripts.js
+++ scripts.js
@@ -1,14 +1,5 @@
-const pessoa = {
-    nome: 'Yuri',
-    idade: 25
-};
-
-for(let chave in pessoa){
-    console.log(chave, pessoa.nome);
-}
-
 const cores = ['vermelho', 'azul', 'verde'];
 
-for(let indice in cores){
-    console.log(indice, cores[indice]);
+for(let cor of cores){
+    console.log(cor);
 }
