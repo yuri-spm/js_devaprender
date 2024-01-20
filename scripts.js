750c66ddf47537dccd325fd2ff2503ca7fc9d011
Yuri do Monte <yspm.developer@gmail.com>
2024-01-19 21:47:43 -0300

    aula-33

---
+++ scripts.js
@@ -0,0 +1,17 @@
+exibirNumerosPrimos(15);
+
+function exibirNumerosPrimos(limite){
+    for(let numero = 2.; numero <= limite; numero++){
+        let ehPrimo = true;
+
+        for(let divisor = 2; divisor < numero; divisor++){
+            if(numero % divisor === 0){
+                ehPrimo = false;
+                break
+            }
+        }
+
+        if(ehPrimo)console.log(numero);
+    }
+
+}
