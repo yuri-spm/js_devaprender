c7e03c5fbfdf0ac1eac01ca3223a2036fc0567bb
Yuri do Monte <yspm.developer@gmail.com>
2024-01-31 23:14:15 -0300

    aula-34

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
+// Oque são objetos?
+// Chave-Valor (Key-Value pair)
+
+
+let marcaCelular = 'LG';
+let tamanhoTelaVertical = 155;
+let tamanhoTelaHorizontal = 75;
+let capacidadeBateria = 5000;
+
+const celular =  {
+    marcaCelular: "Asus",
+    tamanhoTela: {
+        vertical: 155,
+        horizontal: 75
+    },
+    capacidadeBateria: 5000,
+    ligar: function(){
+        console.log("Fazendo ligação ..")
+    }
+}
+
+celular.ligar();
+
+
 function criarCeular(marcaCelular, tamanhoTela, capacidadeBateria ){
     return celular =  {
         marcaCelular,
@@ -10,5 +34,5 @@ function criarCeular(marcaCelular, tamanhoTela, capacidadeBateria ){
     
 }
 
-celular1 = criarCeular('Samsung', 5.5, 5000);
+criarCeular('Samsung', 5.5, 5000);
 console.log(celular1);
